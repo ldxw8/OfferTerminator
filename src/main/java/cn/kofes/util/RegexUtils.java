06d99ba4506f4849a7b71c1bb56ff1173a9fb335
ldxw8 <707474179@qq.com>
2019-09-05 23:32:26 +0800

    修改注释格式

--- src/main/java/cn/kofes/util/RegexUtils.java
+++ src/main/java/cn/kofes/util/RegexUtils.java
@@ -2,16 +2,16 @@ package cn.kofes.util;
 
 /*
  * author: Kofe
- * blog  : https://www.kofes.cn
- * time  : 2019/08/30
- * desc  :
+ * blog : https://www.kofes.cn
+ * time : 2019/08/30
+ * desc:
  * Java 正则表达式参考：https://www.runoob.com/java/java-regular-expressions.html
  */
 public class RegexUtils {
 
     /**
      * 数字匹配篇
-     **/
+     */
 
     // 通用数值字符识别- 科学计数法，如："5e2","0.1e2", ".5e2", "-.5e10", "10E+10" 和 "-1E-16"
     final static public String REGEX_DIGITAL_SCIENTIFIC = "[+|-]?[0-9]*[.]?[0-9]*[e|E]{1}[+|-]?[0-9]+";
