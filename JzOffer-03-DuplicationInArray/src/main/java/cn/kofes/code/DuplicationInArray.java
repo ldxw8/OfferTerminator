06973a4b3e56dfcb85e08c1d532a5c4cf8c3a768
ldxw8 <707474179@qq.com>
2019-09-05 22:13:09 +0800

    查找数组重复的数字-下标法和哈希表法

--- JzOffer-03-DuplicationInArray/src/main/java/cn/kofes/code/DuplicationInArray.java
+++ JzOffer-03-DuplicationInArray/src/main/java/cn/kofes/code/DuplicationInArray.java
@@ -19,11 +19,128 @@ package cn.kofes.code;
  * </pre>
  */
 public class DuplicationInArray {
-    public boolean duplicate(int numbers[],int length,int [] duplication) {
-        return false;
+    /**
+     * 请找出数组中任意一个重复的数字 (下标筛数法)
+     * 下标筛数学法决定了数组中数字的取值范围是 [0, n-1]
+     * @param numbers 整型数组
+     * @param length 数组的长度
+     * @param duplication 返回任意重复的一个数字，赋值给 duplication[0]
+     * @return True 输入有效且数组中存在重复的数字；False 其他情况
+     */
+    public static boolean duplicate(int numbers[],int length,int [] duplication) {
+
+        boolean isDuplicate  = false;
+        boolean isAllPositive  = true;
+
+        if (  null != numbers && null != duplication ) {
+            if ( length == numbers.length ) {
+                // 下标筛数学法决定了数组中数字的取值范围是 [0, n-1]
+                for (int number : numbers) {
+                   if (number < 0 || number >= numbers.length) {
+                       isAllPositive = false;
+                       break;
+                   }
+                }
+
+                if ( isAllPositive ) {
+                    for (int i = 0; i < numbers.length; i++) {
+                        while ( numbers[i] != i ) {
+                            if ( numbers[i] == numbers[numbers[i]] ) {
+                                duplication[0] = numbers[i];
+                                isDuplicate = true;
+                                break; // 跳出最临近的循环体 -- while
+                            }
+
+                            // 位运算：swap(numbers[i], numbers[numbers[i]]);
+                            int tmp = numbers[i];
+                            numbers[i] = numbers[i] ^ numbers[tmp];
+                            numbers[tmp] = numbers[i] ^ numbers[tmp];
+                            numbers[i] = numbers[i] ^ numbers[tmp];
+
+                            // 中间变量交换两值
+                            // int tmp = numbers[numbers[i]];
+                            // numbers[numbers[i]] = numbers[i];
+                            // numbers[i] = tmp;
+                        }
+
+                        if ( isDuplicate ) break; // 跳出最临近的循环体 -- for
+                    }
+                }
+
+            }
+        }
+
+        return isDuplicate;
     }
 
-    public void getDuplicationDigitalInArray() {
+    /**
+     * 请找出数组中任意一个重复的数字 (哈希表筛数法)
+     * 数组中数字的取值范围是 [0, Integer.MAX_VALUE]
+     * @param numbers 整型数组
+     * @param length 数组的长度
+     * @param duplication 返回任意重复的一个数字，赋值给 duplication[0]
+     * @return True 输入有效且数组中存在重复的数字；False 其他情况
+     */
+    public static boolean duplicateWithHash(int numbers[],int length,int [] duplication) {
 
+        boolean isDuplicate  = false;
+        boolean isAllPositive  = true;
+        // 构建一个哈希表，利用冲突的特点，结合重复元素判定的方式，快速找出
+        int[] hashTable = new int[length];
+
+        if ( isAllPositive ) {
+            if (  null != numbers && null != duplication ) {
+                if ( length == numbers.length ) {
+
+                    // 检索输入的数组中是非都为正整数
+                    for (int i = 0, d = 1; i < numbers.length; i++) {
+
+                        hashTable[i] = -1; // 初始化 HashTable
+
+                        if ( numbers[i] < 0 ) {
+                            isAllPositive = false;
+                            break;
+                        }
+                    }
+
+                    for (int i = 0, d = 1, f = 1; i < numbers.length; i++) {
+
+                        // 哈希函数
+                        int hKey = numbers[i] % length;
+                        int rhKey = 0;
+
+                        // 解决冲突 -- 双哈希函数探测法
+                        while ( -1 != hashTable[hKey] ) {
+
+                            // 发生了冲突，检验冲突位置是否是相同元素
+                            if ( hashTable[hKey] == numbers[i] ) {
+                                duplication[0] = numbers[i];
+                                isDuplicate = true;
+                                break;
+                            }
+
+                            rhKey = d^2;
+                            rhKey *= f;
+
+                            // 信号量改变
+                            if( d <=  length/2 ) {
+                                d += 1;
+                            }
+                            f *= -1;
+
+                            hKey = (hKey + rhKey*d) % length;
+
+                        }
+
+                        hashTable[hKey] = numbers[i];
+
+                        if ( isDuplicate ) break; // 跳出最临近的循环体 -- for
+                    }
+                }
+            }
+        }
+
+        return isDuplicate;
     }
+
 }
---
+++ JzOffer-03-DuplicationInArray/src/test/java/cn/kofes/code/DuplicationInArrayTest.java
@@ -0,0 +1,55 @@
+package cn.kofes.code;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DuplicationInArrayTest {
+
+    @Test
+    public void duplicate() {
+
+        // 传入数组的引用，使得运算数据返回主函数中
+        int[]  result = {-1};
+
+        Assert.assertEquals( DuplicationInArray.duplicate(null, 10, result), false);
+        Assert.assertEquals( result[0], -1);
+
+        Assert.assertEquals( DuplicationInArray.duplicate(null, 10, null), false);
+        Assert.assertEquals( result[0], -1);
+
+        int[]  arrays0 = {1, 2, 3, 5, 6, 4, 2, 3};
+        Assert.assertEquals( DuplicationInArray.duplicate(arrays0, 5, result), false);
+        Assert.assertEquals( result[0], -1);
+
+        int[]  arrays1 = {1, 2, -1, 5, 10, 4, 2, 3};
+        Assert.assertEquals( DuplicationInArray.duplicate(arrays1, arrays1.length, result), false);
+        Assert.assertEquals( result[0], -1);
+
+        int[]  arrays2 = {1, 2, 3, 3, 2};
+        Assert.assertEquals( DuplicationInArray.duplicate(arrays2, arrays2.length, result), true);
+        Assert.assertEquals( result[0], 2);
+
+        int[]  arrays3 = {2,4,2,1,4};
+        Assert.assertEquals( DuplicationInArray.duplicate(arrays3, arrays3.length, result), true);
+        Assert.assertEquals( result[0], 2);
+    }
+
+    @Test
+    public void duplicateWithHash() {
+
+        // 传入数组的引用，使得运算数据返回主函数中
+        int[]  result = {-1};
+
+        Assert.assertEquals( DuplicationInArray.duplicateWithHash(null, 10, result), false);
+        Assert.assertEquals( result[0], -1);
+
+        int[]  arrays = {2,4,8,1,4,3, 6, 2, 4};
+        Assert.assertEquals( DuplicationInArray.duplicateWithHash(arrays, arrays.length, result), true);
+
+        int[]  arrays1 = {2,4,8,1,56, 3, 6, 7, 90, 21, 7, 33, 35, 26, 210, 10, 102, 64, 37, 0, 2};
+        Assert.assertEquals( DuplicationInArray.duplicateWithHash(arrays1, arrays1.length, result), true);
+
+    }
+}
