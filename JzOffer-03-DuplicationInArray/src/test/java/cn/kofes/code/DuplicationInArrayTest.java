e2ba468d6a0f79e2cd7450050e1c284711136429
ldxw8 <707474179@qq.com>
2019-09-05 22:56:10 +0800

    查找数组重复的数字-哈希表法-修复无效值 BUG

--- JzOffer-03-DuplicationInArray/src/test/java/cn/kofes/code/DuplicationInArrayTest.java
+++ JzOffer-03-DuplicationInArray/src/test/java/cn/kofes/code/DuplicationInArrayTest.java
@@ -45,11 +45,18 @@ public class DuplicationInArrayTest {
         Assert.assertEquals( DuplicationInArray.duplicateWithHash(null, 10, result), false);
         Assert.assertEquals( result[0], -1);
 
-        int[]  arrays = {2,4,8,1,4,3, 6, 2, 4};
-        Assert.assertEquals( DuplicationInArray.duplicateWithHash(arrays, arrays.length, result), true);
+        Assert.assertEquals( DuplicationInArray.duplicateWithHash(null, 10, null), false);
+        Assert.assertEquals( result[0], -1);
 
-        int[]  arrays1 = {2,4,8,1,56, 3, 6, 7, 90, 21, 7, 33, 35, 26, 210, 10, 102, 64, 37, 0, 2};
-        Assert.assertEquals( DuplicationInArray.duplicateWithHash(arrays1, arrays1.length, result), true);
+        int[]  arrays1 = {1, 2, -1, 5, 10, 4, 2, 3};
+        Assert.assertEquals( DuplicationInArray.duplicateWithHash(arrays1, arrays1.length, result), false);
+        Assert.assertEquals( result[0], -1);
+
+        int[]  arrays2 = {2,4,8,1,4,3, 6, 2, 4};
+        Assert.assertEquals( DuplicationInArray.duplicateWithHash(arrays2, arrays2.length, result), true);
+
+        int[]  arrays3 = {2,4,8,1,56, 3, 6, 7, 90, 21, 7, 33, 35, 26, 210, 10, 102, 64, 37, 0, 2};
+        Assert.assertEquals( DuplicationInArray.duplicateWithHash(arrays3, arrays3.length, result), true);
 
     }
 }
