98b922a4373dac8765d156f460079f0f81808ab2
ldxw8 <707474179@qq.com>
2019-09-06 12:02:31 +0800

    添加哈希表查找算法

---
+++ src/main/java/cn/kofes/struct/HashSearch.java
@@ -0,0 +1,33 @@
+package cn.kofes.struct;
+
+/*
+ * author: Kofe
+ * blog : https://www.kofes.cn
+ * time : 2019/09/06
+ * desc: 哈希表查找 ( 储存结构以及相关算法 )
+ */
+public class HashSearch {
+
+    /**
+     * 顺序结构的哈希表 ( 用于存储的哈希表 )
+     * @param numbers
+     * @param hashTable
+     * @param hashSize
+     * @param conflict
+     */
+    public void  HashSearch(int[] numbers,  int hashTable, int hashSize, int conflict) {
+
+    }
+
+    /**
+     * 从顺序结构的哈希表中查找目标数字
+     * @param obj 需要寻找的目标数字
+     * @return 若找到则返回下标 i，其他情况返回 -1
+     */
+    public int searchInHash(int obj) {
+        int result =  -1;
+
+        return result;
+    }
+
+}
