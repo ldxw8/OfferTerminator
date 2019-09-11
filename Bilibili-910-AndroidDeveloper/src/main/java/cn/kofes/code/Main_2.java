8d119975a6df5ef05ef52dc3838ca35a66bf79ae
ldxw8 <707474179@qq.com>
2019-09-11 10:57:45 +0800

    初步通过本地 IDE 编码 (有待测试系统的用例检验)

--- Bilibili-910-AndroidDeveloper/src/main/java/cn/kofes/code/Main_2.java
+++ Bilibili-910-AndroidDeveloper/src/main/java/cn/kofes/code/Main_2.java
@@ -9,9 +9,9 @@ import java.util.Scanner;
  * time  : 2019/09/10
  * desc  :
  * ==============================================
- * 题目
+ * Bilibili 2020 届秋季校园招聘 -- Android 方向笔试卷（二）-- 编程 2
  * ==============================================
- * 描述
+ * 题目描述：检索两字符是否有包含或者相等关系
  * ==============================================
  * </pre>
  */
@@ -27,7 +27,7 @@ public class Main_2 {
         if ( null != s1 && null != s2 ) {
             if ( s1.matches(regex) && s2.matches(regex) ) {
                 if ( !s1.equals(s2) ) {
-                    result = isEqual(s1, s2);
+                    result = isEqualStr(s1, s2);
                 } else {
                     result = true;
                 }
@@ -38,27 +38,44 @@ public class Main_2 {
 
     }
 
-    public static boolean isEqual(String  s1, String s2) {
+    public static boolean isEqualStr(String  s1, String s2) {
 
         char[] str1 = s1.toCharArray();
         char[] str2 = s2.toCharArray();
+        int choice  =  (str1.length >= str2.length)?0:1;
 
-        if (str1.length <= str2.length) {
-            // str1 是子串
-            for ( int i = 0;i<str2.length;i++ ) {
-                for (int j  = 0;j<str1.length;j++) {
+        boolean result = false;
 
+        switch (choice) {
+            case 0: // str2 为较小长度的子串
+                result = isEqualCharArray(str1, str2);
+                break;
+            case 1: // str1 为较小长度的子串
+                result = isEqualCharArray(str2, str1);
+                break;
+        }
+        return result;
+    }
+
+    /**
+     * 检索两字符数组是否有包含或者相等关系
+     * @param former 表示长度较大的字符数组
+     * @param latter 长度较小的字符数组
+     * @return 若 former 和 latter 有包含或者相等关系，返回 True；其他  False
+     */
+    public static boolean isEqualCharArray(char[] former, char[] latter) {
+        boolean result = false;
+        for (int i = 0, j = 0; i < former.length; i++) {
+            if (former[i] == latter[j]) {
+                if (latter.length == j + 1) {
+                    result = true;
+                    break;
                 }
-            }
-        } else {
-            // str2 是子串
-            for ( int i = 0;i<str1.length;i++ ) {
-                for (int j  = 0;j<str2.length;j++) {
-
-                }
+                j += 1;
+            } else {
+                j = 0;
             }
         }
-
-        return false;
+        return result;
     }
 }
--- Bilibili-910-AndroidDeveloper/src/main/java/cn/kofes/code/Main_3.java
+++ Bilibili-910-AndroidDeveloper/src/main/java/cn/kofes/code/Main_3.java
@@ -9,9 +9,24 @@ import java.util.Scanner;
  * time  : 2019/09/10
  * desc  :
  * ==============================================
- * 题目
+ * Bilibili 2020 届秋季校园招聘 -- Android 方向笔试卷（二）-- 编程 3
  * ==============================================
- * 描述
+ * 题目描述：
+ * 有 N 件物品和一个容量为 V 的背包。第 i 件物品的价值是 C[i]，重量是 W[i]。
+ * 求解将哪些物品装入背包可使价值总和最大。
+ * 输入描述：
+ * 输入第一行数目 N V ( 1<= N <= 500 && 1 <= V <= 10000 )
+ * 输入 N 行，输入两个数字 C W 代表价值和重量 ( 1<= V <= 50000 && 1 <= V <= 10000 )
+ * 输出描述：输出最大价值
+ * Input:
+ * 5 10
+ * 8 6
+ * 10 4
+ * 4 2
+ * 5 4
+ * 5 3
+ * Output:
+ * 19
  * ==============================================
  * </pre>
  */
@@ -19,8 +34,8 @@ public class Main_3 {
 
     public static void main(String[] args) {
             Scanner scanner = new Scanner( System.in );
-            int capacity  = scanner.nextInt();
             int digital  =  scanner.nextInt();
+            int capacity  = scanner.nextInt();
 
             if ( capacity >=1 && capacity <= 500 && digital >=1 && digital <= 10000 ) {
                 int[]  w = new int[digital + 1];
@@ -31,7 +46,7 @@ public class Main_3 {
                     w[i] = scanner.nextInt();
                     v[i] = scanner.nextInt();
 
-                    if ( (w[i] >=1 && w[i] <= 50000) || (v[i] >= 1 && v[i] <= 10000) ) {
+                    if ( (w[i] <1 && w[i] > 50000) || (v[i] <1 && v[i] > 10000) ) {
                         System.out.println(0);
                         return;
                     }
@@ -44,13 +59,13 @@ public class Main_3 {
                         if (w[i] > j) {
                             dp[i][j] = dp[i-1][j];
                         } else {
-                            dp[i][j] = Math.max(dp[i-1][j],dp[i-1][j-w[i]] + v[i] );
+                            dp[i][j] = Math.max( dp[i-1][j],dp[i-1][j-w[i]] + v[i] );
                         }
                     }
                 }
                 System.out.println(dp[digital][capacity]);
             } else {
-
+                System.out.println(0);
             }
     }
 }
