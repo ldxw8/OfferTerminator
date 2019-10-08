de4fda2e24e3334d79fe6064c2d494fe8bbefa7b
ldxw8 <707474179@qq.com>
2019-10-09 01:38:09 +0800

    添加题目描述

---
+++ Leetcode-03-LongestSubStrLength/src/main/java/cn.kofes.code/LongestSubStrLength.java
@@ -0,0 +1,22 @@
+package cn.kofes.code;
+
+/**
+ * <pre>
+ * author: Kofe
+ * blog  : https://www.kofes.cn
+ * time  : 2019/10/09
+ * desc  :
+ * ==============================================
+ * Leetcode-03-无重复字符的最长子串 (https://leetcode-cn.com/problems/longest-substring-without-repeating-characters/)
+ * ==============================================
+ * 给定一个字符串，请你找出其中不含有重复字符的最长子串的长度。
+ * ==============================================
+ * </pre>
+ */
+public class LongestSubStrLength {
+
+    public int lengthOfLongestSubstring(String s) {
+
+    }
+
+}
