e77d2e8b16d2930a40e13a5ad13b5e219922d097
ldxw8 <707474179@qq.com>
2019-10-09 14:26:51 +0800

    添加测试用例

--- Leetcode-03-LongestSubStrLength/src/test/java/cn/kofes/code/LongestSubStrLengthTest.java
+++ Leetcode-03-LongestSubStrLength/src/test/java/cn/kofes/code/LongestSubStrLengthTest.java
@@ -8,6 +8,10 @@ public class LongestSubStrLengthTest {
     @Test
     public void lengthOfLongestSubstring() {
 
+        Assert.assertEquals(new LongestSubStrLength().lengthOfLongestSubstring(null), 0);
+        Assert.assertEquals(new LongestSubStrLength().lengthOfLongestSubstring(""), 0);
+        Assert.assertEquals(new LongestSubStrLength().lengthOfLongestSubstring("d"), 1);
+        Assert.assertEquals(new LongestSubStrLength().lengthOfLongestSubstring("dd"), 1);
         Assert.assertEquals(new LongestSubStrLength().lengthOfLongestSubstring("dvdf"), 3);
         Assert.assertEquals(new LongestSubStrLength().lengthOfLongestSubstring("pwwkew"), 3);
         Assert.assertEquals(new LongestSubStrLength().lengthOfLongestSubstring("abcabcbb"), 3);
